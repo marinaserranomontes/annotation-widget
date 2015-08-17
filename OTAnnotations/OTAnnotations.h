5c2db3d77fb905983209351515164c1d1a6e7271
Trevor Boyer <trevor.boyer@elite-dev.com>
2015-08-17 10:31:35 -0400

    Initial Commit

---
+++ OTAnnotations/OTAnnotations.h
@@ -0,0 +1,19 @@
+//
+//  OTAnnotations.h
+//  OTAnnotations
+//
+//  Created by Trevor Boyer on 8/17/15.
+//  Copyright (c) 2015 TokBox, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OTAnnotations.
+FOUNDATION_EXPORT double OTAnnotationsVersionNumber;
+
+//! Project version string for OTAnnotations.
+FOUNDATION_EXPORT const unsigned char OTAnnotationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OTAnnotations/PublicHeader.h>
+
+
